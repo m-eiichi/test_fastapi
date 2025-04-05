d30d530e2955b4d2a181dc6f9937078a08cf59c0
webyosupernow@gmail.com <webyosupernow@gmail.com>
2025-04-05 22:15:05 +0900

    save

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -7,7 +7,7 @@ GENERATED_FILE="$GENERATED_DIR/__init__.py"
 if [ ! -f "$GENERATED_FILE" ]; then
     echo "sqlacodegen を初回実行中..."
     mkdir -p "$GENERATED_DIR"
-    # sqlacodegen postgresql://user:password@db:5432/mydb --outfile "$GENERATED_FILE"
+    sqlacodegen "$DATABASE_URL" --outfile "$GENERATED_FILE" #SQLAlchemyモデルを自動的に生成
 else
     echo "sqlacodegen は既に実行済み"
 fi
